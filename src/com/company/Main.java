1c491c984150acba048bb2372becff4b253c604b
Ralph F <RaftsDev@gmail.com>
2020-09-20 21:52:32 -0400

    Feat: Check Series.nSum.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import com.company.mylibraries.Series;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Testing Series library
+
+        System.out.println("******** Sum ************");
+        for(int i = 0; i == 10; i++){
+            System.out.println(Series.nSum(i));
+        }
     }
 }
