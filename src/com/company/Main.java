6196ce12bf6a79ca69162d56258b7bdc5d6391f5
Ralph F <RaftsDev@gmail.com>
2020-09-20 21:58:25 -0400

    Feat: Add output Series.factorial().

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,5 +11,10 @@ public class Main {
         for(int i = 0; i <= 10; i++){
             System.out.println(Series.nSum(i));
         }
+
+        System.out.println("******** Factorial ************");
+        for(int i = 0; i <= 10; i++){
+            System.out.println(Series.factorial(i));
+        }
     }
 }
